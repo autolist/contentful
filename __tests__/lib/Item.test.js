5062db07f85618e7dfd783f81e356b7430313e98
Asher Cohen <asherlc@asherlc.com>
2018-08-24 12:51:12 -0700

    Add tests

--- __tests__/lib/Item.test.js
+++ __tests__/lib/Item.test.js
@@ -15,21 +15,77 @@ jest.mock('contentful', () => {
   };
 });
 
+class Article extends Item {
+  static fields = ['title', 'body', 'slug'];
+
+  static relationships = ['parentPage', 'categories', 'author'];
+}
+class Page extends Item {
+  static fields = ['slug'];
+}
+class Category extends Item {
+  static fields = ['name'];
+}
+
+class Author extends Item {
+  static fields = ['name'];
+}
+
+Item.classes.article = Article;
+Item.classes.page = Page;
+Item.classes.category = Category;
+Item.classes.author = Author;
+
 describe('Item', () => {
   describe('static', () => {
     describe('getClass', () => {
       it('returns a class based on the content type', () => {
-        const mockClass = jest.fn();
-        Item.classes.mock_class = mockClass;
-
-        expect(Item.getClass('mock_class')).toBe(mockClass);
+        expect(Item.getClass('article')).toBe(Article);
       });
     });
 
     describe('findAll', () => {
-      it('does the thing', async () => {
-        const item = await Item.findAll();
-        expect(item).toBe({});
+      it('returns an array of models', async () => {
+        const articles = await Article.findAll();
+        expect(articles).toHaveLength(1);
+      });
+    });
+
+    describe('find', () => {
+      it('returns one models', async () => {
+        const article = await Article.find();
+        expect(article.constructor).toBe(Article);
+      });
+    });
+
+    describe('toJSON', () => {
+      let article;
+
+      beforeEach(async () => {
+        article = await Article.find();
+      });
+
+      it('returns an object with the correct fields', async () => {
+        expect(article.toJSON()).toMatchObject({
+          slug: 'article-title',
+          body:
+            'The body of the content (//images.contentful.com/6tuem73u73an/1QXPHgEwraG00mugw08uSe/d198d126ba821c50ce94d7a3302ae267/TestImage1.jpeg) (//images.ctfassets.net/6tuem73u73an/1QXPHgEwraG00mugw08uSe/d198d126ba821c50ce94d7a3302ae267/TestImage2.jpeg)',
+          title: 'Article title'
+        });
+      });
+
+      it('returns an object with the correct relationships', async () => {
+        expect(article.toJSON().parentPage).toMatchObject({
+          slug: 'ford-mustang'
+        });
+      });
+
+      it('returns an array with the correct plural relationships', async () => {
+        expect(article.toJSON().categories).toMatchObject([
+          {
+            name: 'News & Analysis'
+          }
+        ]);
       });
     });
   });

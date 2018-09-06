151e303f6f318c9f6475e6ae46bddc4d7d3f5118
Asher Cohen <asherlc@asherlc.com>
2018-09-06 14:15:41 -0700

    Fix handling of empty assets

--- __tests__/lib/Asset.test.js
+++ __tests__/lib/Asset.test.js
@@ -20,6 +20,34 @@ describe('Asset', () => {
     it('returns the url', () => {
       expect(asset.url).toEqual('google.com');
     });
+
+    describe('when fields is an empty object', () => {
+      beforeEach(() => {
+        const data = {
+          sys: {
+            space: {
+              sys: { type: 'Link', linkType: 'Space', id: '6tuem73u73an' }
+            },
+            id: '6yuHRmH6KsSqEIKA2KAYOY',
+            type: 'Asset',
+            createdAt: '2018-09-04T18:24:54.378Z',
+            updatedAt: '2018-09-04T18:24:54.378Z',
+            environment: {
+              sys: { id: 'master', type: 'Link', linkType: 'Environment' }
+            },
+            revision: 1,
+            locale: 'es'
+          },
+          fields: {}
+        };
+
+        asset = new Asset(data);
+      });
+
+      it('returns undefined', () => {
+        expect(asset.url).toBe(undefined);
+      });
+    });
   });
 
   describe('id', () => {
